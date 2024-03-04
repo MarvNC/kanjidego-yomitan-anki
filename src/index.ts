3adf616e99a02b2c4827a7232b098e72e361fcd5
MarvNC <rotrobmin@gmail.com>
2024-03-03 18:26:30 -0800

    Add image processing

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,93 @@
 import { scrapeAllPagesData } from './scrapePageData';
 import { buildDictionary } from './buildDictionary';
 import { termData } from './types';
-import { IMAGES_DIRECTORY, KANJI_IMAGE_URL } from './constants';
+import {
+  IMAGES_DIRECTORY,
+  KANJI_IMAGE_URL,
+  PROCESSED_DIRECTORY,
+} from './constants';
 import path from 'path';
 import fs from 'fs';
+import sharp from 'sharp';
 
 (async () => {
   const termDataArr = await scrapeAllPagesData();
   await scrapeAllImages(termDataArr);
+  // await processAllImages();
   await buildDictionary(termDataArr);
 })();
 
 async function scrapeAllImages(termDataArr: termData[]) {
-  const imageDir = path.join(process.cwd(), IMAGES_DIRECTORY);
+  const sourceImageDir = path.join(process.cwd(), IMAGES_DIRECTORY);
+  const processedImageDir = path.join(process.cwd(), PROCESSED_DIRECTORY);
+
+  const processImagePromises = [];
+
   for (const termData of termDataArr) {
     const levelID = termData.termInfo.問題ID;
     if (!levelID) {
-      throw new Error('termInfo.問題ID must be defined');
+      throw new Error(
+        `Error on term ${termData.termReading.term}: 問題ID must be defined`
+      );
     }
     const imageURL = KANJI_IMAGE_URL(levelID);
-    const imageFilePath = path.join(imageDir, `${levelID}.png`);
+    const imageFilePath = path.join(sourceImageDir, `${levelID}.png`);
 
     // Check if image already exists
     if (fs.existsSync(imageFilePath)) {
       console.log(`Image already exists at ${imageFilePath}`);
-      continue;
+    } else {
+      console.log(`Scraping image from ${imageURL}`);
+      const response = await fetch(imageURL);
+      const buffer = await response.arrayBuffer();
+      if (!fs.existsSync(sourceImageDir)) {
+        fs.mkdirSync(sourceImageDir);
+      }
+      fs.writeFileSync(imageFilePath, Buffer.from(buffer));
     }
 
-    console.log(`Scraping image from ${imageURL}`);
-    const response = await fetch(imageURL);
-    const buffer = await response.arrayBuffer();
-    if (!fs.existsSync(imageDir)) {
-      fs.mkdirSync(imageDir);
-    }
-    fs.writeFileSync(imageFilePath, Buffer.from(buffer));
+    // Add promise to process the image
+    processImagePromises.push(
+      processImage(sourceImageDir, processedImageDir, `${levelID}.png`)
+    );
   }
+  return Promise.all(processImagePromises);
+}
+
+/**
+ * Process a single image by trimming transparency.
+ * The processed image will be saved in the processed directory.
+ * @param sourceImageDir The directory containing the source images.
+ * @param processedImageDir The directory where the processed images will be saved.
+ * @param imageName The name of the image file to process.
+ */
+async function processImage(
+  sourceImageDir: string,
+  processedImageDir: string,
+  imageName: string
+) {
+  const sourceImagePath = path.join(sourceImageDir, imageName);
+  const processedImagePath = path.join(processedImageDir, imageName);
+
+  // Create the processed directory if it doesn't exist
+  if (!fs.existsSync(processedImageDir)) {
+    fs.mkdirSync(processedImageDir);
+  }
+
+  // Check if the image has already been processed
+  if (fs.existsSync(processedImagePath)) {
+    console.log(`Image already processed at ${processedImagePath}`);
+    return;
+  }
+
+  console.log(`Processing image at ${sourceImagePath}`);
+  await sharp(sourceImagePath)
+    .trim()
+    .toFile(processedImagePath)
+    .then(() => {
+      console.log(`Processed image saved at ${processedImagePath}`);
+    })
+    .catch((err) => {
+      console.error(`Error processing image at ${sourceImagePath}: ${err}`);
+    });
 }
