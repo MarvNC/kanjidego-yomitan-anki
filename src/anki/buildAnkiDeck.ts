66ddfd649eb6e40e7920dcce98d5447ffb46c414
MarvNC <rotrobmin@gmail.com>
2024-03-05 22:24:31 -0800

    Refactor names and code to be simpler

--- src/anki/buildAnkiDeck.ts
+++ src/anki/buildAnkiDeck.ts
@@ -8,10 +8,6 @@ import {
   KANJI_DE_GO_NAME,
 } from '../constants';
 
-/**
- * Prints the data to a csv file
- * @param termDataArr
- */
 export async function buildAnkiDeck(termDataArr: termData[]) {
   console.log('Building Anki deck');
 
@@ -22,80 +18,70 @@ export async function buildAnkiDeck(termDataArr: termData[]) {
   const csvWriter = createObjectCsvWriter({
     path: csvPath,
     header: [
-      { id: 'term', title: '単語' },
-      { id: 'origTerm', title: '元単語' },
-      { id: 'id', title: '問題ID' },
-      { id: 'reading', title: '読み方' },
-      { id: 'altReadings', title: '別解' },
-      { id: 'altForms', title: '別表記' },
-      { id: 'meaning', title: '意味' },
-      { id: 'notes', title: '追記' },
-      { id: 'level', title: 'レベル' },
-      { id: 'image', title: '画像' },
-      { id: 'croppedImage', title: '切り抜き画像' },
-      { id: 'tags', title: 'Tags' },
+      { id: '単語', title: '単語' },
+      { id: '元単語', title: '元単語' },
+      { id: '問題ID', title: '問題ID' },
+      { id: '読み方', title: '読み方' },
+      { id: '別解', title: '別解' },
+      { id: '別表記', title: '別表記' },
+      { id: '意味', title: '意味' },
+      { id: '追記', title: '追記' },
+      { id: 'レベル', title: 'レベル' },
+      { id: '画像', title: '画像' },
+      { id: '切り抜き画像', title: '切り抜き画像' },
+      { id: 'Tags', title: 'Tags' },
     ],
   });
 
-  // Loop thru
   for (const termData of termDataArr) {
-    // Print to csv
     const { term, reading } = termData.termReading;
-    const {
-      別表記: altForms,
-      別解: altReadings,
-      意味: meaning,
-      追記: notes,
-      問題ID: id,
-    } = termData.termInfo;
-    const altFormsString = altForms ? altForms.join('・') : '';
-    const altReadingsString = altReadings ? altReadings.join('・') : '';
+    const { 別表記, 別解, 意味, 追記, 問題ID } = termData.termInfo;
 
     const level = termData.termLevel;
 
-    const image = id ? `<img src="${IMAGE_NAME(id)}">` : '';
-    const croppedImage = id ? `<img src="${CROPPED_IMAGE_NAME(id)}">` : '';
-
     let tags = `${KANJI_DE_GO_NAME}-${level}`;
 
-    const records = [];
-
     const record: {
-      id?: string;
-      term: string;
-      origTerm?: string;
-      reading: string;
-      meaning?: string;
-      altReadings: string;
-      altForms: string;
-      notes?: string;
-      level: string;
-      image: string;
-      croppedImage: string;
-      tags?: string;
+      問題ID?: string;
+      単語: string;
+      元単語?: string;
+      読み方: string;
+      意味?: string;
+      別解: string;
+      別表記: string;
+      追記?: string;
+      レベル: string;
+      画像: string;
+      切り抜き画像: string;
+      Tags?: string;
     } = {
-      id,
-      term,
-      reading,
-      meaning,
-      altReadings: altReadingsString,
-      altForms: altFormsString,
-      notes,
-      level,
-      image,
-      croppedImage,
-      tags,
+      問題ID,
+      単語: term,
+      読み方: reading,
+      意味,
+      別解: 別解 ? 別解.join('・') : '',
+      別表記: 別表記 ? 別表記.join('・') : '',
+      追記,
+      レベル: level,
+      画像: 問題ID ? `<img src="${IMAGE_NAME(問題ID)}">` : '',
+      切り抜き画像: 問題ID ? `<img src="${CROPPED_IMAGE_NAME(問題ID)}">` : '',
+      Tags: tags,
     };
+
+    const records: (typeof record)[] = [];
 
     records.push({ ...record });
 
-    // Add alternates with tag '別表記'
-    record.origTerm = term;
+    record.元単語 = term;
     tags += ` ${KANJI_DE_GO_NAME}-別表記`;
 
-    for (const altForm of altForms ?? []) {
-      // Set term to altTerm and set origTerm
-      records.push({ ...record, term: altForm, origTerm: term, tags });
+    for (const altForm of 別表記 ?? []) {
+      records.push({
+        ...record,
+        単語: altForm,
+        元単語: term,
+        Tags: tags,
+      });
     }
 
     await csvWriter.writeRecords(records);
