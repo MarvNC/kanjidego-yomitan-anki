3ca9935d20ad8bf7495633d3965e9d967c8c07a8
MarvNC <rotrobmin@gmail.com>
2024-03-05 22:43:16 -0800

    Refactor and separate out function for getting records

--- src/anki/buildAnkiDeck.ts
+++ src/anki/buildAnkiDeck.ts
@@ -32,52 +32,53 @@ export async function buildAnkiDeck(termDataArr: termData[]) {
       { id: 'Tags', title: 'Tags' },
     ],
   });
+  const records: csvRecord[] = [];
 
   for (const termData of termDataArr) {
-    const { term, reading } = termData.termReading;
-    const { 別表記, 別解, 意味, 追記, 問題ID } = termData.termInfo;
+    getRecordsForTerm(termData, records);
+  }
+  await csvWriter.writeRecords(records);
+}
+function getRecordsForTerm(termData: termData, records: csvRecord[]) {
+  const { term, reading } = termData.termReading;
+  const { 別表記, 別解, 意味, 追記, 問題ID } = termData.termInfo;
 
-    const level = termData.termLevel;
+  const level = termData.termLevel;
 
-    let tags = `${KANJI_DE_GO_NAME}-${level}`;
+  let tags = `${KANJI_DE_GO_NAME}-${level}`;
 
-    const record: csvRecord = {
-      問題ID,
-      単語: term,
-      読み方: reading,
-      意味,
-      別解: 別解 ? 別解.join('・') : '',
-      別表記: 別表記 ? 別表記.join('・') : '',
-      追記,
-      レベル: level,
-      画像: 問題ID ? `<img src="${IMAGE_NAME(問題ID)}">` : '',
-      切り抜き画像: 問題ID ? `<img src="${CROPPED_IMAGE_NAME(問題ID)}">` : '',
+  const record: csvRecord = {
+    問題ID,
+    単語: term,
+    読み方: reading,
+    意味,
+    別解: 別解 ? 別解.join('・') : '',
+    別表記: 別表記 ? 別表記.join('・') : '',
+    追記,
+    レベル: level,
+    画像: 問題ID ? `<img src="${IMAGE_NAME(問題ID)}">` : '',
+    切り抜き画像: 問題ID ? `<img src="${CROPPED_IMAGE_NAME(問題ID)}">` : '',
+    Tags: tags,
+  };
+
+  records.push({ ...record });
+
+  record.元単語 = term;
+  tags += ` ${KANJI_DE_GO_NAME}-別表記`;
+
+  if (!別表記) return;
+
+  for (const altForm of 別表記 ?? []) {
+    // Remove this alt form from array and add back original term
+    const newAlts = 別表記.filter((alt) => alt !== altForm);
+    newAlts.unshift(term);
+
+    records.push({
+      ...record,
+      単語: altForm,
+      元単語: term,
+      別表記: newAlts.join('・'),
       Tags: tags,
-    };
-
-    const records: (typeof record)[] = [];
-
-    records.push({ ...record });
-
-    record.元単語 = term;
-    tags += ` ${KANJI_DE_GO_NAME}-別表記`;
-
-    if (別表記) {
-      for (const altForm of 別表記) {
-        // Remove this alt form from array and add back original term
-        const newAlts = 別表記.filter((alt) => alt !== altForm);
-        newAlts.unshift(term);
-
-        records.push({
-          ...record,
-          単語: altForm,
-          元単語: term,
-          別表記: newAlts.join('・'),
-          Tags: tags,
-        });
-      }
-    }
-
-    await csvWriter.writeRecords(records);
+    });
   }
 }
